a4faeaeab88625b860ba9d44fe97ebdb73f8c605
Igor <progr2000@gmail.com>
2025-03-15 15:12:11 +0200

    update elements test added tests: -test_web_table_add_person -test_web_table_search_person

--- generator/generator.py
+++ generator/generator.py
@@ -1,12 +1,19 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
-faker_ru = Faker("ru_Ru")
+faker_ua = Faker("uk_UA")
 
 def generated_person():
     yield Person(
-        full_name= faker_ru.first_name() + " " + faker_ru.last_name(),
-        email= faker_ru.email(),
-        current_address= faker_ru.address(),
-        permanent_address= faker_ru.address(),
+        full_name= faker_ua.first_name() + " " + faker_ua.last_name(),
+        first_name= faker_ua.first_name(),
+        last_name= faker_ua.last_name(),
+        age= random.randint(10,80),
+        salary= random.randint(1000, 5000),
+        department= faker_ua.job(),
+        email= faker_ua.email(),
+        current_address= faker_ua.address(),
+        permanent_address= faker_ua.address(),
     )
