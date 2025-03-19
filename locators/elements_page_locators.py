6f9f8f542ad9c14601b24ea258b2f8e1b7a962e2
Igor <progr2000@gmail.com>
2025-03-19 21:12:09 +0200

    update elements test added  class: - TestUploadAndDownload added tests: - test_upload_fil - test_download_file

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -65,6 +65,13 @@ class LinksPageLocators:
     SIMPLE_LINK = (By.CSS_SELECTOR, "a[id='simpleLink']")
     BAD_REQUEST = (By.CSS_SELECTOR, "a[id='bad-request']")
 
+class UploadAndDownloadPageLocators:
+    DOWNLOAD_FILE = (By.CSS_SELECTOR, "a[id='downloadButton']")
+    UPLOAD_FILE = (By.CSS_SELECTOR, "input[id='uploadFile']")
+    UPLOADED_RESULT = (By.CSS_SELECTOR, "p[id = 'uploadedFilePath']")
+
+
+
 
 
 
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,8 @@
 import random
 import time
 
-from pages.elements_page import TestBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonPage, LinksPage
+from pages.elements_page import TestBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonPage, LinksPage, \
+    UploadAndDownloadPage
 
 
 class TestElements:
@@ -111,4 +112,22 @@ class TestElements:
             links_page.open()
             response_code = links_page.check_broken_link("https://demoqa.com/bad-request")
             assert response_code == 400, "The link is correct or status code son 400"
+
+    class TestUploadAndDownload:
+
+        def test_upload_file(self, driver):
+            upload_download_page = UploadAndDownloadPage(driver, "https://demoqa.com/upload-download")
+            upload_download_page.open()
+            file_name, result = upload_download_page.upload_file()
+            assert file_name == result, "The file has not been uploaded"
+
+
+
+        def test_download_file(self, driver):
+            upload_download_page = UploadAndDownloadPage(driver, "https://demoqa.com/upload-download")
+            upload_download_page.open()
+            check = upload_download_page.download_file()
+            assert check is True, "The file has not been downloaded"
+
+
 
