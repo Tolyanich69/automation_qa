54cdc597171e7c8f4b4bdee7cf3eaec7601bb031
Igor <progr2000@gmail.com>
2025-03-20 10:27:31 +0200

    update elements test added  class: - TestDynamicPropertiesPege added tests: - test_dynamic_properties - test_appear_button - test_enable_button

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -70,6 +70,12 @@ class UploadAndDownloadPageLocators:
     UPLOAD_FILE = (By.CSS_SELECTOR, "input[id='uploadFile']")
     UPLOADED_RESULT = (By.CSS_SELECTOR, "p[id = 'uploadedFilePath']")
 
+class DynamicPropertiesPageLocators:
+    COLOR_CHANGE_BUTTON = (By.CSS_SELECTOR, "button[id='colorChange']")
+    VISIBLE_AFTER_FIVE_SEC_BUTTON = (By.CSS_SELECTOR, "button[id='visibleAfter']")
+    ENABLE_BUTTON = (By.CSS_SELECTOR, "button[id = 'enableAfter']")
+
+
 
 
 
