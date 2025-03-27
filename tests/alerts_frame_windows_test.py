71001cc0710c0c514ea5a2edba9f498d5e67b53e
Igor <progr2000@gmail.com>
2025-03-27 22:37:36 +0200

    added class TestNestedFramesPage
    added test
    -test_nested_frames

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,6 +1,6 @@
 
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage, FramesPage, NestedFramesPage
 
 
 class TestAlertsFrameWindow:
@@ -56,3 +56,12 @@ class TestAlertsFrameWindow:
             result_frame2 = frame_page.check_frame("frame2")
             assert result_frame1 == ['This is a sample page', '500px', '350px'], "The frame does not exist"
             assert result_frame2 == ['This is a sample page', '100px', '100px'], "The frame does not exist"
+
+    class TestNestedFramesPage:
+
+        def test_nested_frames(self, driver):
+            nested_frames_page = NestedFramesPage(driver, "https://demoqa.com/nestedframes")
+            nested_frames_page.open()
+            parent_text, child_text = nested_frames_page.check_nested_frame()
+            assert parent_text == "Parent frame", "Nested frame goes not exist"
+            assert child_text == "Child Iframe", "Nested frame goes not exist"
